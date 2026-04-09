d6246a602fc0f68439820c8ddb9eabd333c1a2e4
Ahmad Ayoub <matoayoub2002@gmail.com>
2026-04-09 20:12:25 +0300

    name

--- dashboard/app.py
+++ dashboard/app.py
@@ -20,7 +20,7 @@ st.divider()
 col1, col2 = st.columns(2)
 
 with col1:
-    st.page_link("pages/try_It.py", label="Try the Salary Predictor", icon="🎯")
+    st.page_link("pages/try_it.py", label="Try the Salary Predictor", icon="🎯")
 
 with col2:
     st.page_link("pages/Insights.py", label="View Market Insights", icon="📊")
